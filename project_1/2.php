0c12cac661ef66369690840a601dacf1dced9d88
DebashishK03 <114876969+DebashishK03@users.noreply.github.com>
2023-01-17 01:03:05 +0530

    Add files via upload

---
+++ project_1/2.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <link rel="shortcut icon" type="image/png" href="B:\chem notes\IMG-20220710-WA0006.jpg">
+    </head>
+    <body>
+        <button><a href="https://www.google.com" target=""> Click here to see more </a></button>
+    </body>
+</html>
